080dc7704f57372ac6f1b63ae30eefcc4b77bc4e
Bolton Bailey <bolton.bailey@gmail.com>
2017-02-04 22:21:50 -0800

    One Hot Encoding added to preprocess the input.

--- analysis.py
+++ analysis.py
@@ -3,6 +3,7 @@
 import csv
 import numpy as np
 from sklearn import tree
+from sklearn.preprocessing import OneHotEncoder
 from collections import Counter
 
 import random
@@ -32,6 +33,37 @@ with open('train_2008.csv', 'r') as dest_f:
     X_train = np.array([map(int, datum[:-1]) for datum in data])
     y_train = np.array([int(datum[-1]) for datum in data])
 
+    categorical_indices = []
+
+    for i in range(len(X_train[0])):
+
+        cnt = {}
+        for X in X_train:
+            if X[i] not in cnt:
+                cnt[X[i]] = len(cnt)
+
+        print lables[i], cnt
+
+        if len(cnt) < 150:
+            categorical_indices.append(i)
+            for X in X_train:
+                X[i] = cnt[X[i]]
+
+
+
+
+    enc = OneHotEncoder(categorical_features=categorical_indices)
+    enc.fit(X_train)
+
+
+
+    print X_train[0]
+    X_train = enc.transform(X_train).toarray()
+    for foo in X_train[0]:
+        print ">", foo
+
+
+
 
 
 
@@ -49,6 +81,8 @@ y_test = y_train[:6400] - 1
 X_train = X_train[6400:]
 y_train = y_train[6400:] - 1
 
+print float(sum(y_test))/len(y_test)
+
 ## Transforms output to one-hot encoding
 y_train = keras.utils.np_utils.to_categorical(y_train, 2)
 y_test = keras.utils.np_utils.to_categorical(y_test, 2)
@@ -62,8 +96,10 @@ print X_train.shape
 
 ## Create your own model here given the constraints in the problem
 model = Sequential()
-model.add(Dense(500, input_shape=(382,)))
-model.add(Activation('relu'))
+model.add(Dense(1000, input_shape=(len(X_train[0]),)))
+model.add(Activation('sigmoid'))
+# model.add(Dense(20))
+# model.add(Activation('relu'))
 ## We use one-hot encoding, we now put 10 on our output layer
 model.add(Dense(2))
 model.add(Activation('softmax'))
@@ -79,9 +115,11 @@ fit = model.fit(X_train, y_train, batch_size=128, nb_epoch=10,
     verbose=1)
 
 ## Printing the accuracy of our model, according to the loss function specified in model.compile above
-score = model.evaluate(X_test, y_test, verbose=0)
+score = model.evaluate(X_test, y_test, verbose=1)
 print('Test score:', score[0])
 print('Test accuracy:', score[1])
+
+print model.predict_on_batch(X_test)
 
 
 
