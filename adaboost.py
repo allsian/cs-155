7b3ea8c444ecec91da9ade6dd9b5c6bf9b813f79
Bolton Bailey <bolton.bailey@gmail.com>
2017-02-07 00:36:57 -0800

    Added adaboost output file

--- adaboost.py
+++ adaboost.py
@@ -1,6 +1,6 @@
 from sklearn import ensemble
 import sklearn_classifier
 
-for i in range(100, 1500, 100):
+for i in range(25, 1500, 25):
     clf = ensemble.AdaBoostClassifier(n_estimators=i)
     sklearn_classifier.classify(clf, 'adaboost/adaboost-%d' % i)
