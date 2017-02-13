f5366f246090203aa82e6b5159541605f804a6a3
Bolton Bailey <bolton.bailey@gmail.com>
2017-02-12 19:11:50 -0800

    Wrote concluding remarks. Wrote code to get exact proportion of voters in the test data.

--- get_data.py
+++ get_data.py
@@ -7,10 +7,10 @@ def get_data():
 
   with open('train_2008.csv', 'r') as dest_f:
 
-  data_iter = csv.reader(dest_f, delimiter = ",", quotechar = '"')
-  data = [data for data in data_iter]
+    data_iter = csv.reader(dest_f, delimiter = ",", quotechar = '"')
+    data = [data for data in data_iter]
 
-  # print data[0]
+      # print data[0]
   # print data[1]
   # print data[2]
 
@@ -24,32 +24,32 @@ def get_data():
 
   categorical_indices = []
 
-  for i in range(len(X_train[0])):
+  # for i in range(len(X_train[0])):
+  #
+  #     cnt = {}
+  #     for X in X_train:
+  #         if X[i] not in cnt:
+  #             cnt[X[i]] = len(cnt)
+  #
+  #     print lables[i], cnt
+  #
+  #     if len(cnt) < 150:
+  #         categorical_indices.append(i)
+  #         for X in X_train:
+  #             X[i] = cnt[X[i]]
+  #
+  #
+  #
+  #
+  # enc = OneHotEncoder(categorical_features=categorical_indices)
+  # enc.fit(X_train)
 
-      cnt = {}
-      for X in X_train:
-          if X[i] not in cnt:
-              cnt[X[i]] = len(cnt)
-
-      print lables[i], cnt
-
-      if len(cnt) < 150:
-          categorical_indices.append(i)
-          for X in X_train:
-              X[i] = cnt[X[i]]
 
 
-
-
-  enc = OneHotEncoder(categorical_features=categorical_indices)
-  enc.fit(X_train)
-
-
-
-  print X_train[0]
-  X_train = enc.transform(X_train).toarray()
-  for foo in X_train[0]:
-      print ">", foo
+  # print X_train[0]
+  # X_train = enc.transform(X_train).toarray()
+  # for foo in X_train[0]:
+  #     print ">", foo
 
 
 
@@ -57,10 +57,10 @@ def get_data():
 
 
   # Normalize data
-  row_means = X_train.mean(axis=1)
-  row_stds = X_train.std(axis=1)
-  X_train = X_train - row_means[:, np.newaxis]
-  X_train = X_train / row_stds[:, np.newaxis]
+  # row_means = X_train.mean(axis=1)
+  # row_stds = X_train.std(axis=1)
+  # X_train = X_train - row_means[:, np.newaxis]
+  # X_train = X_train / row_stds[:, np.newaxis]
 
 
   # Split into a training set and a test set for validation
@@ -72,3 +72,13 @@ def get_data():
 
 
   return X_train, y_train, X_test, y_test
+
+
+if __name__ == '__main__':
+    X_train, y_train, X_test, y_test= get_data()
+
+    y_train = list(y_train) + list(y_test)
+
+    num_data = len(y_train)
+    num_voters = len([y for y in y_train if y == 1])
+    print num_data, num_voters, float(num_voters)/num_data
