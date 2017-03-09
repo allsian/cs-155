773feacd3059813a3f168517a0a9e6b20bd79d33
Bolton Bailey <bolton.bailey@gmail.com>
2017-03-08 16:13:33 -0800

    Added genre specific bar charts.

--- Miniproject-3/unstacked_bar_charts.py
+++ Miniproject-3/unstacked_bar_charts.py
@@ -10,11 +10,16 @@ user_ids_vector = list(data[0])
 movie_ids_vector = list(data[1])
 rating_vector = list(data[2])
 
+genre_data = np.genfromtxt('project3data/movies.txt', delimiter = '\t', dtype=None)
+
+
 
 
 rating_values = np.arange(1, 5+1)
 
-rating_counts = [rating_vector.count(r) for r in rating_values]
+relevant_ratings_list = [r for (m_id, r) in zip(movie_ids_vector, rating_vector)]
+
+rating_counts = [relevant_ratings_list.count(r) for r in rating_values]
 
 
 print rating_values
@@ -25,30 +30,12 @@ width = 0.35
 fig, ax = plt.subplots()
 rects1 = ax.bar(rating_values, rating_counts, width, color='r')
 
-# women_means = (25, 32, 34, 20, 25)
-# women_std = (3, 5, 2, 3, 3)
-# rects2 = ax.bar(ind + width, women_means, width, color='y', yerr=women_std)
-
 # add some text for labels, title and axes ticks
 ax.set_ylabel('Scores')
 ax.set_title('Scores by group and gender')
 ax.set_xticks(rating_values + width / 2)
 ax.set_xticklabels(('1', '2', '3', '4', '5'))
 
-# ax.legend((rects1[0], rects2[0]), ('Men', 'Women'))
-
-
-# def autolabel(rects):
-#     """
-#     Attach a text label above each bar displaying its height
-#     """
-#     for rect in rects:
-#         height = rect.get_height()
-#         ax.text(rect.get_x() + rect.get_width()/2., 1.05*height,
-#                 '%d' % int(height),
-#                 ha='center', va='bottom')
-#
-# autolabel(rects1)
 
 plt.title("Number of Ratings Across all Movies")
 plt.xlabel("Rating")
@@ -56,3 +43,36 @@ plt.ylabel("Number of Ratings")
 
 
 plt.savefig("visualizations/all_ratings_bar_chart.pdf")
+
+
+
+for genre_index, genre_name in zip(range(3, 21), ["Action", "Adventure", "Animation", "Childrens", "Comedy", "Crime", "Documentary", "Drama", "Fantasy", "Film-Noir", "Horror", "Musical", "Mystery", "Romance", "Sci-Fi", "Thriller", "War", "Western"]):
+
+    rating_values = np.arange(1, 5+1)
+
+    relevant_ratings_list = [r for (m_id, r) in zip(movie_ids_vector, rating_vector) if genre_data[m_id-1][genre_index] == 1]
+
+    rating_counts = [relevant_ratings_list.count(r) for r in rating_values]
+
+
+    print rating_values
+    print rating_counts
+
+    width = 0.35
+
+    fig, ax = plt.subplots()
+    rects1 = ax.bar(rating_values, rating_counts, width, color='r')
+
+    # add some text for labels, title and axes ticks
+    ax.set_ylabel('Scores')
+    ax.set_title('Scores by group and gender')
+    ax.set_xticks(rating_values + width / 2)
+    ax.set_xticklabels(('1', '2', '3', '4', '5'))
+
+
+    plt.title("Number of Ratings Across %s Movies" % genre_name)
+    plt.xlabel("Rating")
+    plt.ylabel("Number of Ratings")
+
+
+    plt.savefig("visualizations/%s_ratings_bar_chart.pdf" % genre_name)
